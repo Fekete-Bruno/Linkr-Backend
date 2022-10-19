8e75feb081537951c8e77d28561bdf31dc53c71f
felipelyra3 <felipelyra3@gmail.com>
2022-10-19 11:18:58 -0300

    Fix Sessions

--- src/middlewares/schemaValidation.middlewares.js
+++ src/middlewares/schemaValidation.middlewares.js
@@ -4,7 +4,7 @@ async function SignUpSchema(req, res, next) {
     const signUpSchema = Joi.object({
         name: Joi.string().empty().max(255).required(),
         email: Joi.string().empty().email().max(255).required(),
-        img: Joi.string().pattern(new RegExp('/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/')).allow(null).required(),
+        img: Joi.string().pattern(new RegExp('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})')).allow(null).required(),
         password: Joi.string().empty().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
     });
 
