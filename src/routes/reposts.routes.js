dab86c617b2802ec6b0e76e9e42802c266af638a
Fekete-Bruno <brunohffekete@gmail.com>
2022-10-28 17:02:34 -0300

    fix: import

--- src/routes/reposts.routes.js
+++ src/routes/reposts.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import * as repostController from "../controllers/repost.controllers.js";
 import { repostMiddleware } from "../middlewares/reposts.middlewares.js";
+import { listUserPosts } from "../repositories/users.repositories.js";
 
 const router = express.Router();
 
 router.post("/repost/:id", repostMiddleware, repostController.repost);
 
-router.get("/userposts/:id", usersController.listUserPosts);
+router.get("/userposts/:id", listUserPosts);
 
 export default router;
