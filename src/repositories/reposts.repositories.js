c18b9ada46819cc2f2effe6ed917db49468c8642
savioferraz <savio.ferraz88@gmail.com>
2022-10-27 21:40:19 -0300

    feat: user page reposts

--- src/repositories/reposts.repositories.js
+++ src/repositories/reposts.repositories.js
@@ -18,4 +18,46 @@ async function checkRepost(userId, postId) {
   );
 }
 
-export { insertRepost, checkRepost };
+async function listUserReposts(userId) {
+  return connection.query(
+    `
+    SELECT
+      posts.id AS "postId",
+      posts.url,
+      posts.description,
+      posts."userId" AS "ownerId",
+      u1.name AS "ownerName",
+      u1.img AS "ownerImg",
+      u3.name AS "reposterName",
+      COALESCE("repostsData"."repostCount", 0) AS "reposts",
+	    COUNT ("likeData"."postId") AS "likes",
+      JSON_AGG (
+        JSON_BUILD_OBJECT(
+          'userId', "likeData"."userId",
+          'name', "likeData".username
+        )) AS "likeArray"
+    FROM posts
+    JOIN users AS u1
+      ON posts."userId"=u1.id
+    LEFT JOIN (
+        SELECT "userId", "postId", name AS "username"
+        FROM likes
+        LEFT JOIN users
+          ON likes."userId"=users.id
+        GROUP BY likes."postId",likes."userId",users.name
+        ) AS "likeData" ON posts.id = "likeData"."postId"
+    LEFT JOIN (
+        SELECT "userId", "postId", COUNT("postId") AS "repostCount" 
+        FROM reposts 
+        GROUP BY "postId", "userId"
+        ) AS "repostsData"
+        ON posts.id = "repostsData"."postId"
+    LEFT JOIN users AS u3
+      ON "repostsData"."userId"=u3.id
+    WHERE posts."userId"=$1 OR "repostsData"."userId"=$1 
+    GROUP BY posts.id, u1.name, u1.img, u3.name, "repostCount"`,
+    [userId]
+  );
+}
+
+export { insertRepost, checkRepost, listUserReposts };
