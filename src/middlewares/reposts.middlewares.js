02ebf0b9798004450e35b90256cc101ae9404d32
savioferraz <savio.ferraz88@gmail.com>
2022-10-28 13:29:30 -0300

    repost update pull

--- src/middlewares/reposts.middlewares.js
+++ src/middlewares/reposts.middlewares.js
@@ -1,12 +1,22 @@
-import { checkRepost } from "../repositories/reposts.repositories.js";
+import {
+  checkRepost,
+  checkPostOwner,
+} from "../repositories/reposts.repositories.js";
 
 async function repostMiddleware(req, res, next) {
   try {
     const userId = res.locals.searchToken[0].userId;
     const { id } = req.params;
     const sameRepost = (await checkRepost(userId, id)).rows;
+    const postOwner = (await checkPostOwner(userId, id)).rows;
+
     if (sameRepost.length !== 0) {
       res.status(406).send("You already reposted this post");
+      return;
+    }
+
+    if (postOwner.length !== 0) {
+      res.status(406).send("You can't repost your own post");
       return;
     }
 
